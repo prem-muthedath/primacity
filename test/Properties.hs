5d62e7f918c0cf8d22ca2c8fca4cf00bc8132cc5
Prem Muthedath <prem.muthedath@gmail.com>
2021-03-26 17:02:59 +0530

    1. test/Tests.hs: refactored, + comments; 2. test/Properties.hs: + comments; 3. reanmed, edited test/*.txt files.

--- test/Properties.hs
+++ test/Properties.hs
@@ -1,10 +1,15 @@
+-- | QuickCheck properties.
+-- author: Prem Muthedath
+
 module Properties where
 
 import Primacity (primeFactors)
 
+-- | check if product of prime factors is indeed equal to the number.
 factorsProduct :: Int -> Bool
 factorsProduct  n = n == product (primeFactors n)
 
+-- | check if factors are all primes.
 factorsPrime :: Int -> Bool
 factorsPrime n = let a = [(x, y) | x <- primeFactors n, y <- [2..(x-1)]]
                      b = [ a1 | (a1, b1) <- a, a1 `mod` b1 == 0 ]
--- test/Tests.hs
+++ test/Tests.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
+-- | Test code.
+-- author: Prem Muthedath
+
 module Tests (run) where
 
 import Test.QuickCheck
@@ -9,73 +12,84 @@ import Data.List (zipWith4, stripPrefix)
 import Properties (factorsProduct, factorsPrime)
 import Primacity (primacitys)
 
-parseLine :: (String -> String) -> String -> String
+-- | parse a line of text.
+parseLine :: (String -> Maybe String) -> String -> String
 parseLine _ []          = []   -- ignore blank
 parseLine _ ('-':'-':_) = []   -- ignore comment
-parseLine f line = f line
+parseLine f line = case (f line) of
+                      Just x  -> x
+                      Nothing -> error $ "parse failed at line: " ++ line
 
-error' :: String -> String
-error' line = error $ "parse failed at line: " ++ line
-
-parseInputLine :: String -> String
-parseInputLine line
-          | valid     = replace
-          | otherwise = error' line
+-- | return primacity test case as a comma-delimited string from a line of text.
+-- line pattern: `x y z` where x, y, z are numbers with any number of digits.
+-- example: `1673990 4964281 4`. we want to extract this pattern as "x,y,z".
+testCase :: String -> Maybe String
+testCase line
+          | valid     = Just $ replace
+          | otherwise = Nothing
   where valid :: Bool
-        valid = length (words line) == 3  -- pattern: "156 287 6"
+        valid = length (words line) == 3  -- example pattern: "156 287 6"
         replace :: String
         replace = concatMap (\x -> if x == ' ' then "," else [x]) line
 
-parseOutputLine :: String -> String
--- pattern match `Case #x: ` where x is a number in [1..100]
-parseOutputLine line = parse' $ stripPrefix "Case #" line
-            where parse' :: Maybe String -> String
-                  parse' (Just xs) = get' xs
-                  parse' Nothing   = error' line
-                  get' :: String -> String
-                  get' ys = let zs     = dropWhile (/=':') ys
-                                prefix = take 2 zs
-                                valid  = (zs /= ys) && (prefix == ": ")
-                            in if valid then drop 2 zs else error' line
+-- | return expected primacity value from a line of text.
+-- line pattern: `Case #x: y` where `x` is a number in [1..100] & `y`, the 
+-- primacity, can have any number of digits. example: `Case #10: 691209`.
+anExpected :: String -> Maybe String
+anExpected line = do
+      xs <- stripPrefix "Case #" line
+      ys <- anExpected' xs
+      return ys
+  where anExpected' :: String -> Maybe String
+        anExpected' ys = let zs     = dropWhile (/=':') ys
+                             prefix = take 2 zs
+                             valid  = (zs /= ys) && (prefix == ": ")
+                         in if valid then Just $ drop 2 zs else Nothing
 
-get :: String -> (String -> String) -> [String]
-get contents f = nonempty . map (\line -> parseLine f line) $ allLines
+-- | parse contents, parsing each line using a supplied parsing function.
+parse :: String -> (String -> Maybe String) -> [String]
+parse contents f = nonempty . map (\line -> parseLine f line) $ allLines
   where allLines :: [String]
         allLines = lines contents
         nonempty :: [String]-> [String]
         nonempty = filter (/="")
 
-type Input = (Int, Int, Int)
+type TestCase = (Int, Int, Int)
 
-getInput :: IO [Input]
-getInput = do
-  contents <- readFile "./test/input.txt"
-  let pLines = get contents parseInputLine
-  return $ map (\line -> read $ "(" ++ line ++ ")" :: Input) pLines
+-- | return list of primacity test cases, each of type `TestCase`.
+testCases :: IO [TestCase]
+testCases = do
+  contents <- readFile "./test/test-cases.txt"
+  let pLines = parse contents testCase
+  return $ map (\line -> read $ "(" ++ line ++ ")" :: TestCase) pLines
 
-getOutput :: IO [Int]
-getOutput = do
-  contents <- readFile "./test/expected-output.txt"
-  let pLines = get contents parseOutputLine
+-- | return expected primacity values.
+expected :: IO [Int]
+expected = do
+  contents <- readFile "./test/expected-primacity-values.txt"
+  let pLines = parse contents anExpected
   return $ map (\x -> read x :: Int) pLines
 
-test :: IO ()
-test = do
-  xs <- getInput
-  zs <- getOutput
+-- | run primacity tests.
+primacityTests :: IO ()
+primacityTests = do
+  xs <- testCases
+  zs <- expected
   let ys :: [Int] = primacitys xs
       results     = zipWith4 (\n x y z -> (n, x, y, z)) [1..] xs ys zs
   printResults results
 
+-- | some type synonyms to make the code clear.
 type ItemNo   = Int
 type Actual   = Int
 type Expected = Int
 
-printResults :: [(ItemNo, Input, Actual, Expected)] -> IO ()
+-- | format & print primacity test results.
+printResults :: [(ItemNo, TestCase, Actual, Expected)] -> IO ()
 printResults vals = do
-  putStrLn "########################################################################"
-  putStrLn "TEST RESULTS -- Case # : input (a, b, k) | actual | expected | PASS/FAIL"
-  putStrLn "########################################################################"
+  putStrLn "##################################################################################"
+  putStrLn "PRIMACITY TEST RESULTS -- Case # : input (a, b, k) | actual | expected | PASS/FAIL"
+  putStrLn "##################################################################################"
   mapM_(\(n, x, y, z) -> putStrLn $
       "Case #" ++  show n ++ ": " ++
       show x ++ " | " ++
@@ -83,16 +97,18 @@ printResults vals = do
       show z ++ " | "  ++
       if (y==z) then "PASS" else "FAIL") vals
 
-qctest :: IO ()
-qctest = do
-  putStrLn "########################################################################"
+-- | run quick check tests.
+qcTests :: IO ()
+qcTests = do
+  putStrLn "##################################################################################"
   putStrLn "QuickCheck Test Results"
-  putStrLn "########################################################################"
+  putStrLn "##################################################################################"
   quickCheck factorsProduct
   quickCheck factorsPrime
 
+-- | run all tests.
 run :: IO ()
 run = do
-  test
-  qctest
+  primacityTests
+  qcTests
 
